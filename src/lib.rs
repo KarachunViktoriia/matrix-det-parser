19390b547cfafdd3f942ea83b49cda83160d3551
KarachunViktoriia <v.karachun@ukma.edu.ua>
2023-11-23 05:02:28 +0200

    initial commit

---
+++ src/lib.rs
@@ -0,0 +1,42 @@
+extern crate nalgebra as na;
+
+use std::io;
+
+pub fn read_matrix() -> na::DMatrix<f64> {
+    println!("Введіть розмірність матриці:");
+    let mut input = String::new();
+    io::stdin().read_line(&mut input)
+        .expect("Не вдалося прочитати рядок");
+    let n: usize = match input.trim().parse() {
+        Ok(value) => value,
+        Err(_) => {
+            eprintln!("Некоректне введення для розмірності матриці");
+            std::process::exit(1);
+        }
+    };
+
+    println!("Введіть елементи матриці (розділені пробілами):");
+
+    let matrix_data: Vec<f64> = (0..n)
+        .flat_map(|_| {
+            let mut row = String::new();
+            io::stdin()
+                .read_line(&mut row)
+                .expect("Не вдалося прочитати рядок");
+
+            row.split_whitespace()
+                .filter_map(|s| {
+                    let parsed = s.parse().ok();
+                    if parsed.is_none() {
+                        eprintln!("Помилка парсингу для: {:?}", s);
+                    }
+                    parsed
+                })
+                .collect::<Vec<_>>() // Збираємо значення у вектор
+        })
+        .collect();
+
+    println!("Debug: {:?}", matrix_data); // Доданий вивід для відладки
+
+    na::DMatrix::from_row_slice(n, n, &matrix_data)
+}
---
+++ src/main.rs
@@ -0,0 +1,32 @@
+use matrix_det_parser:: read_matrix;
+
+fn print_help() {
+    println!("Використання:");
+    println!("  matrix-det-parser help         Вивести додаткову інформацію");
+    println!("  matrix-det-parser credits      Вивести дані");
+}
+
+fn print_credits() {
+    println!("Обчислення детермінанту матриці");
+    println!("Автор: KarachunViktoriia");
+    println!("Версія: 0.1.0");
+}
+
+fn main() {
+    let args: Vec<String> = std::env::args().collect();
+
+    if args.len() == 1 {
+        let matrix = read_matrix();
+        let determinant = matrix.determinant();
+        println!("Детермінант матриці: {}", determinant);
+    } else {
+        match args[1].as_str() {
+            "help" => print_help(),
+            "credits" => print_credits(),
+            _ => {
+                eprintln!("Невідома команда. Використовуйте 'help' для допомоги.");
+                std::process::exit(1);
+            }
+        }
+    }
+}
